94398462e6aa6f0ada48af268b404148d89127d5
lucasschindhelm <114084013+lucasschindhelm@users.noreply.github.com>
2023-03-13 17:16:41 +0100

    Update compilescript.sh
    Added information to args for test_client.c

--- compilescript.sh
+++ compilescript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 gcc -o testclient test_client.c -lm `pkg-config --cflags --libs libmodbus`
 sudo ./testclient "192.168.0.1" "0" "1" "502" "0" "0" "0"
-#args ip slave_id debug port anpassung_registernummer(-!) byteordnerändern? bitorder_in statuswort
+# these parameters are correct for OHS-Elcxtrolyser
+#args ip slave_id debug port anpassung_registernummer(base address)(-!) byteordnerändern( little endian? bitorder_in statuswort
